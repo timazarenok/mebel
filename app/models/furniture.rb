9192757704b2d5ec9507fbd844886ab09dd7347f
timazarenok <timofey.zarenok@gmail.com>
2021-05-13 17:29:38 +0300

    added search and removed notice

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -3,10 +3,13 @@ class Furniture < ApplicationRecord
   belongs_to :category
   has_one_attached :image
   has_many :orders, dependent: :delete_all
+  scope :filter_by_name, -> (name) { where("name like ?", "#{name}%")}
+  scope :filter_by_desc, -> (data) { where("description like ?", "%#{data}%")}
+  scope :filter_by_price, -> (price) { where price: price }
 
   def self.search(search)
     if search
-      Array(self.where(name: search))
+      (Array(self.filter_by_desc(search)) + Array(self.filter_by_name(search)) + Array(self.filter_by_price(search))).uniq
     else
       Furniture.all
     end
